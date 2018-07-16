8d67103be9fe33d48b77e6cfe842f5d20587144e
Boularak Souheyb <cs_boularak@esi.dz>
2018-07-16 14:56:44 +0100

    problems fixes on your review

--- music/models.py
+++ music/models.py
@@ -10,7 +10,6 @@ class Album(models.Model):
     def get_absolute_url(self):
         reverse('music:detail',kwargs={'pk':self.pk})
 
-
     def __str__(self):
         return self.album_title +' , '+ self.artist
 
@@ -18,6 +17,7 @@ class Song(models.Model) :
     album=models.ForeignKey(Album,on_delete=models.CASCADE)
     file_type=models.CharField(max_length=10)
     song_title=models.CharField(max_length=250)
-    is_favorite=models.BooleanField(default=False)
+    is_favorite=models.BooleanField()
+
     def __str__(self):
         return self.song_title
